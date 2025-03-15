d753b0f5ac8fcf84e56b7eb0f3b10e5ad68a744c
Nasir <nasirsyedahmed13@gmail.com>
2025-03-15 13:33:38 -0500

    Made script to create DB and added first value

--- database/setup.py
+++ database/setup.py
@@ -1,16 +1,47 @@
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env file
-load_dotenv()
-
-db_url = os.getenv("DATABASE_URL")
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from db_settings import db_url
 
 engine = create_engine(db_url)
+
+Base = declarative_base()
+
+class Name(Base):
+    __tablename__ = "names"
+    
+    first_name = Column(String(50), nullable=False, primary_key=True)
+    last_name = Column(String(50), nullable=False, primary_key=True)
+    language = Column(String(50), nullable=False)
+    country = Column(String(50), nullable=False)
+    first_name_meaning = Column(String(200))
+    last_name_meaning = Column(String(200))
+    gender = Column(String(10), nullable=False)
+
 
 try:
     with engine.connect() as connection:
         print("Connected to PostgreSQL database successfully!")
 except Exception as e:
     print(f"Error: {e}")
+
+
+engine = create_engine(db_url)
+
+Base.metadata.create_all(engine)
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+new_name = Name(
+    first_name="Omar", 
+    last_name="Al-Fayez",
+    language="Arabic",
+    country="Jordan",
+    first_name_meaning="Long-lived",
+    last_name_meaning="The victorious",
+    gender="Male"
+    )
+session.add(new_name)
+session.commit()
+
+print("Created Database")
