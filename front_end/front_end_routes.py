a57d44613fccf321eb52628985cff3e58f75dc40
Nasir <nasirsyedahmed13@gmail.com>
2025-03-17 23:44:06 -0500

    added more front-end pieces

--- front_end/front_end_routes.py
+++ front_end/front_end_routes.py
@@ -1,12 +1,14 @@
-from typing import List
-from litestar import Router, get, post
-from api.models.name import nameResponse, nameRequest
-from api.database import db_functions
+from litestar import Router, get
 from litestar.response import Template
 
 @get("/")
 async def index() -> Template:
     return Template(template_name="index.html")
+
+@get("/arabic-name-generator")
+async def arabic_names() -> Template:
+    return Template(template_name="arabic.html")
+
     
 
-front_end_router = Router(path="", route_handlers=[index])
+front_end_router = Router(path="", route_handlers=[index, arabic_names])
